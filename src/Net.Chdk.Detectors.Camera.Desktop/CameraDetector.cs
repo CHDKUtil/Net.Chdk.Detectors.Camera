597dba4cc683930d3c83ab689dae821ec7afb1dc
Dmitry Shechtman <damnian@gmail.com>
2017-04-19 23:13:26 +0300

    GetCameraFromMetadata

--- src/Net.Chdk.Detectors.Camera.Desktop/CameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera.Desktop/CameraDetector.cs
@@ -3,13 +3,20 @@ using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.Exif.Makernotes;
 using Net.Chdk.Model.Camera;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Directory = MetadataExtractor.Directory;
 
 namespace Net.Chdk.Detectors.Camera
 {
     public sealed class CameraDetector
     {
-        public CameraInfo GetCamera(System.IO.Stream stream)
+        public CameraInfo GetCamera(string driveLetter)
+        {
+            return GetCameraFromMetadata(driveLetter);
+        }
+
+        public CameraInfo GetCamera(Stream stream)
         {
             var metadata = ImageMetadataReader.ReadMetadata(stream);
             if (metadata.Count == 0)
@@ -20,6 +27,19 @@ namespace Net.Chdk.Detectors.Camera
                 Base = GetBase(metadata),
                 Canon = GetCanon(metadata),
             };
+        }
+
+        private CameraInfo GetCameraFromMetadata(string driveLetter)
+        {
+            var metadataPath = Path.Combine(driveLetter, "METADATA");
+            var cameraPath = Path.Combine(metadataPath, "CAMERA.JSN");
+            if (!File.Exists(cameraPath))
+                return null;
+
+            using (var stream = File.OpenRead(cameraPath))
+            {
+                return JsonObject.Deserialize<CameraInfo>(stream);
+            }
         }
 
         private BaseInfo GetBase(IReadOnlyList<Directory> metadata)
