4329508d0b1af682945090b31dcfc7b6b8ec6606
Dmitry Shechtman <damnian@gmail.com>
2017-04-19 19:00:51 +0300

    Add Console project

---
+++ src/Net.Chdk.Detectors.Camera.Console/Program.cs
@@ -0,0 +1,86 @@
+﻿using Net.Chdk.Model.Camera;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Net.Chdk.Detectors.Camera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                WriteUsage();
+                return;
+            }
+
+            string inputPath = null;
+            string outputPath = null;
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-i":
+                        settings.Formatting = Formatting.Indented;
+                        break;
+                    default:
+                        if (inputPath == null)
+                        {
+                            inputPath = args[i];
+                        }
+                        else if (outputPath == null)
+                        {
+                            outputPath = args[i];
+                        }
+                        else
+                        {
+                            WriteUsage();
+                            return;
+                        }
+                        break;
+                }
+            }
+            try
+            {
+                var cameraInfo = GetCamera(inputPath);
+                cameraInfo.Version = "1.0";
+                Serialize(outputPath, cameraInfo, settings);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        private static CameraInfo GetCamera(string path)
+        {
+            var detector = new CameraDetector();
+            using (var stream = File.OpenRead(path))
+            {
+                return detector.GetCamera(stream);
+            }
+        }
+
+        private static void Serialize(string path, CameraInfo cameraInfo, JsonSerializerSettings settings)
+        {
+            using (var stream = File.OpenWrite(path))
+            using (var writer = new StreamWriter(stream))
+            {
+                JsonSerializer.CreateDefault(settings).Serialize(writer, cameraInfo);
+            }
+        }
+
+        private static void WriteUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine();
+            Console.WriteLine("generate <input-path> <output-path> [-i]");
+            Console.WriteLine();
+            Console.WriteLine("\t<input-path>  Input JPEG file path");
+            Console.WriteLine("\t<output-path> Output camera metadata file path");
+            Console.WriteLine("\t-i            Indented output");
+        }
+    }
+}
