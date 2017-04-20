23e3f2e428ba9ca0eb738021f26a9ff9ff7398ab
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 17:29:52 +0300

    Derive from MetadataDetector

--- src/Net.Chdk.Detectors.Camera.Desktop/MetadataCameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera.Desktop/MetadataCameraDetector.cs
@@ -1,31 +1,20 @@
 ﻿using Microsoft.Extensions.Logging;
 using Net.Chdk.Model.Camera;
-using System.IO;
 
 namespace Net.Chdk.Detectors.Camera
 {
-    sealed class MetadataCameraDetector : ICameraDetector
+    sealed class MetadataCameraDetector : MetadataDetector<MetadataCameraDetector, CameraInfo>, ICameraDetector
     {
-        public ILogger Logger { get; }
-
         public MetadataCameraDetector(ILoggerFactory loggerFactory)
+            : base(loggerFactory)
         {
-            Logger = loggerFactory.CreateLogger<MetadataCameraDetector>();
         }
 
         public CameraInfo GetCamera(string driveLetter)
         {
-            var metadataPath = Path.Combine(driveLetter, "METADATA");
-            var cameraPath = Path.Combine(metadataPath, "CAMERA.JSN");
-            if (!File.Exists(cameraPath))
-                return null;
-
-            Logger.LogInformation("Reading {0}", cameraPath);
-
-            using (var stream = File.OpenRead(cameraPath))
-            {
-                return JsonObject.Deserialize<CameraInfo>(stream);
-            }
+            return GetValue(driveLetter);
         }
+
+        protected override string FileName => "CAMERA.JSN";
     }
 }
