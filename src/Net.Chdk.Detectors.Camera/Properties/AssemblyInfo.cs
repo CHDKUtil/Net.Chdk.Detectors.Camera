342fb34ce215f9292d7b36f290af903ab59ffe62
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 19:27:55 +0300

    Rename Desktop project

--- src/Net.Chdk.Detectors.Camera/Properties/AssemblyInfo.cs
+++ src/Net.Chdk.Detectors.Camera/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Net.Chdk.Detectors.Camera.Desktop")]
+[assembly: AssemblyTitle("Net.Chdk.Detectors.Camera")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dmitry Shechtman")]
