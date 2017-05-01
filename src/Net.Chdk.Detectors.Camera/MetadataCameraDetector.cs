0e86ea216da846d361f1ed80f115f811425fc966
Dmitry Shechtman <damnian@gmail.com>
2017-05-02 02:58:08 +0300

    Extract validator

--- src/Net.Chdk.Detectors.Camera/MetadataCameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera/MetadataCameraDetector.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
 using Net.Chdk.Model.Camera;
 using Net.Chdk.Model.Card;
-using System;
+using Net.Chdk.Validators;
 
 namespace Net.Chdk.Detectors.Camera
 {
     sealed class MetadataCameraDetector : MetadataDetector<MetadataCameraDetector, CameraInfo>, IInnerCameraDetector
     {
-        public MetadataCameraDetector(ILoggerFactory loggerFactory)
-            : base(loggerFactory)
+        public MetadataCameraDetector(IValidator<CameraInfo> validator, ILoggerFactory loggerFactory)
+            : base(validator, loggerFactory)
         {
         }
 
@@ -16,64 +16,9 @@ namespace Net.Chdk.Detectors.Camera
         {
             Logger.LogTrace("Detecting camera from {0} metadata", cardInfo.DriveLetter);
 
-            var camera = GetValue(cardInfo);
-            if (camera == null)
-                return null;
-
-            if (!Validate(camera.Version))
-                return null;
-
-            if (!Validate(camera.Base))
-                return null;
-
-            if (!Validate(camera.Canon))
-                return null;
-
-            return camera;
+            return GetValue(cardInfo);
         }
 
         protected override string FileName => "CAMERA.JSN";
-
-        private static bool Validate(Version version)
-        {
-            if (version == null)
-                return false;
-
-            if (version.Major < 1 || version.Minor < 0)
-                return false;
-
-            return true;
-        }
-
-        private static bool Validate(BaseInfo @base)
-        {
-            if (@base == null)
-                return false;
-
-            if (string.IsNullOrEmpty(@base.Make))
-                return false;
-
-            if (string.IsNullOrEmpty(@base.Model))
-                return false;
-
-            return true;
-        }
-
-        private static bool Validate(CanonInfo canon)
-        {
-            if (canon == null)
-                return false;
-
-            if (canon.ModelId < 0x100000)
-                return false;
-
-            if (canon.FirmwareRevision < 0x1000000)
-                return false;
-
-            if ((canon.FirmwareRevision & 0xffff) < 0x100)
-                return false;
-
-            return true;
-        }
     }
 }
